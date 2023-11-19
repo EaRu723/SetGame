93042aa0fdee0b6f944aabe3c323ad40e90cf2bf
EaRu723 <y.andrearusso@gmail.com>
2023-11-19 18:08:04 -0500

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Andrea Russo on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
