be3e6939f27382644cf4e77452d4187e965b4980
EaRu723 <y.andrearusso@gmail.com>
2023-11-19 21:50:29 -0500

    Refactor BottomActionBar in SetGame with hint and deal button labels

---
+++ SetGame/SetGameView/BottomActionBar.swift
@@ -0,0 +1,18 @@
+//
+//  BottomActionBar.swift
+//  
+//
+//  Created by Andrea Russo on 11/19/23.
+//
+
+import SwiftUI
+
+struct BottomActionBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BottomActionBar()
+}
