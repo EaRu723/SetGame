5cd97a3926ed645550513412d28fa9e582e95b3f
EaRu723 <y.andrearusso@gmail.com>
2023-11-19 22:07:04 -0500

    Created CardGridHelper to optimize LazyVGrid

---
+++ SetGame/CardGridHelper.swift
@@ -0,0 +1,18 @@
+//
+//  CardGridHelper.swift
+//  
+//
+//  Created by Andrea Russo on 11/19/23.
+//
+
+import SwiftUI
+
+struct CardGridHelper: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardGridHelper()
+}
